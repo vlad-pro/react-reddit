00b1cefe4b971be9456f61c1bf43aeb14901ac81
vproskurin <vladimir.proskurin@gmail.com>
2020-09-15 14:01:44 +0300

    ORM configuration and usage

---
+++ server/docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A script to start the container for Postgres
+# Do not forget to change the directory where you would like to the container to put the files.
+
+docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v /Users/vproskurin/work/js_Projects/learning/react/react-reddit/server/data:/var/lib/postgresql/data  postgres
+echo Container Up
+
+# You can use the following scripts to get the directory of the bash script and use 
+# it in the docker run command above or use any arbituary link 
+# DIR="$(dirname "$(readlink -f "$0")")" # for Linux-based systems
+# DIR=$(cd "$(dirname "$0")"; pwd) # for macos
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hi friends!")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "react_reddit",
+    user: "postgres",
+    password: "docker",
+    port: 5432,
+    host: "localhost",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+console.log("Hi friends!");
