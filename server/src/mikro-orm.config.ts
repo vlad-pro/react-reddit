ed74d29c1cb6696761e167f0a343599046c0ba88
vproskurin <vladimir.proskurin@gmail.com>
2020-09-17 11:18:00 +0300

    more mikroorm setup

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join( __dirname,"./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files; js is also included in the pattern
+  },
   entities: [Post],
   dbName: "react_reddit",
   user: "postgres",
@@ -11,4 +16,5 @@ export default {
   host: "localhost",
   type: "postgresql",
   debug: !__prod__,
-} as Parameters<typeof MikroORM.init>[0];
+} as Parameters<typeof MikroORM.init>[0]; // This is some black magic to me right now.
+// It's for TS to understand the type properply when importing, and for VSCode to do the autocompletion
