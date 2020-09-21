2b05db1341a7954b6cd80e20d158e5c2e311cf09
vproskurin <vladimir.proskurin@gmail.com>
2020-09-21 01:32:52 -0700

    Appollo Server Express config finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,38 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up(); // Run the migrations after connecting to the db
 
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+  
+  // This is a test for express. It's a REST api test but we are using GQL in here so it's commented
+  // app.get("/", (_, res) => { // to ignore the request (req) we are using an underscore, looks like go/python
+  //   res.send("hello")
+  // });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
   // ====== creating entities ======
   // const post = orm.em.create(Post, { title: "my first post" });
   // await orm.em.persistAndFlush(post);
-  
+
   // ====== listing entities ======
   // const posts = await orm.em.find(Post, {});
   // console.log(posts);
