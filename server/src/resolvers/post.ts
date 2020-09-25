dc957f57dc26ff50985931fba80e35d80b86e87c
vproskurin <vladimir.proskurin@gmail.com>
2020-09-24 23:52:48 -0700

    mikroORM TypeGraphQL CRUD 3

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,6 +1,7 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+import { title } from "process";
 
 @Resolver()
 export class PostResolver {
@@ -11,11 +12,22 @@ export class PostResolver {
   }
 
   // Find the exact one
-  @Query(() => Post, { nullable: true }) // Some attention to the types (2)
+  @Query(() => Post, { nullable: true }) // Some attention to the types (1)
   post(
     @Arg("id", () => Int) id: number,
     @Ctx() ctx: MyContext
-  ): Promise<Post | null> { // Some attention to the types (2)
+  ): Promise<Post | null> {
+    // Some attention to the types (2)
     return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const post = ctx.em.create(Post, { title });
+    await ctx.em.persistAndFlush(post);
+    return post;
   }
 }
