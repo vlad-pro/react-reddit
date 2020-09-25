c5706cecf803eb16aef3ff2b6eb63c8b6d6acec1
vproskurin <vladimir.proskurin@gmail.com>
2020-09-25 08:43:28 -0700

    mikroORM TypeGraphQL CRUD 4 Last one

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,7 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
-import { title } from "process";
 
 @Resolver()
 export class PostResolver {
@@ -21,6 +20,7 @@ export class PostResolver {
     return ctx.em.findOne(Post, { id });
   }
 
+  // Create new post
   @Mutation(() => Post)
   async createPost(
     @Arg("title", () => String) title: string,
@@ -29,5 +29,37 @@ export class PostResolver {
     const post = ctx.em.create(Post, { title });
     await ctx.em.persistAndFlush(post);
     return post;
+  }
+
+  // Update a post
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  // Delete a post
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+    try {
+      await ctx.em.nativeDelete(Post, { id });
+    } catch {
+      return false;
+    }
+    return true;
   }
 }
