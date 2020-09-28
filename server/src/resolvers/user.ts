40eea1cabf447dc03d32a236a8c05631a3ba3913
vproskurin <vladimir.proskurin@gmail.com>
2020-09-27 22:39:46 -0700

    User login Resolver and Register refactoring

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -6,11 +6,15 @@ import {
   Field,
   InputType,
   Mutation,
+  ObjectType,
   // Query,
   Resolver,
+  UseMiddleware,
 } from "type-graphql";
 import argon2 from "argon2";
 
+// Using this class to pass it as InputType for Resolver functions, so reuse is there.
+// It's possible to do the same with @Arg() decorator in function but clubersome
 @InputType()
 class UsernamePasswordInput {
   @Field()
@@ -19,19 +23,105 @@ class UsernamePasswordInput {
   password: string;
 }
 
+// This is for UserResponse object type to return an error object.
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+// This thing is for login mutation, it will return either user or error.
+// User and errors fields are both optional and nullable. Can be done with TS Union
+@ObjectType()
+class UserResponse {
+  @Field((returns) => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() ctx: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length < 1) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username is empty",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "password should be at least lenth 3",
+          },
+        ],
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = ctx.em.create(User, {
       username: options.username,
       password: hashedPassword,
     });
-    await ctx.em.persistAndFlush(user);
-    return user;
+    try {
+      await ctx.em.persistAndFlush(user);
+    } catch (err) {
+      // This is taken form conssole.log output
+      if (err.code === "23505" || err.detail.includes("already exists")) {
+        // duplicate username error
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already exists",
+            },
+          ],
+        };
+      }
+      // console.log("message: ", err)
+    }
+    return { user };
+  }
+
+  @Mutation((returns) => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() ctx: MyContext
+  ): Promise<UserResponse> {
+    const user = await ctx.em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username does not exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+    return { user }; // return user object, no errors. This satisfies the return type.
   }
 }
